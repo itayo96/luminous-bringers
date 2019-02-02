46927c5bf7e14aeadf01364c9155a005d9e88603
LiadCohen <cohenliad1@walla.com>
2019-02-02 21:01:33 +0200

    Starting point block
    Added a collision to all three scenes to block the player from moving outside of the level (from left side)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -235,7 +235,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator OnDeathAnimation()
     {
-        yield return new WaitForSeconds(0.8f);
+        yield return new WaitForSeconds(0.77f);
 
         Destroy(gameObject);
     }
