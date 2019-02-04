bea1823438d58a0589308202ec39527d3d696487
LiadCohen <cohenliad1@walla.com>
2019-02-04 20:45:41 +0200

    Smoother Controls
    The controller is now smoother and the code is a little bit more clean. Air is now being controlled via Raycast (WOAH!)
    All three prefabs were updated with their animation times. And animators post death transition was changed.
    Health is now being drawn as icon x hp. Added mana draw to the EL.

--- Assets/Scripts/Elementalist/ElementalistController.cs
+++ Assets/Scripts/Elementalist/ElementalistController.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class ElementalistController : PlayerController
 {
+    // Public Stats Members
+    public int manaMaxCount = 3;
+    public float manaRefreshRate = 5f;
+    public Texture2D manaTexture;
+
+    // Private Stats Members
+    private int mana = 3;
+
+    new void Start()
+    {
+        base.Start();
+        mana = manaMaxCount;
+    }
+
     // -----------
     // Controllers
     // -----------
@@ -21,5 +35,11 @@ public class ElementalistController : PlayerController
         base.OnGUI();
 
         // Mana
+        Rect manaIcon = new Rect(Screen.width - 100, 10, 64, 64);
+        for (int i = 1; i <= mana; i++)
+        {
+            GUI.DrawTexture(manaIcon, manaTexture);
+            manaIcon.x -= (manaIcon.width + 10);
+        }
     }
 }
