31076885e6313da8f4e31e0700b52b43fed45408
LiadCohen <cohenliad1@walla.com>
2019-02-04 20:45:41 +0200

    Smoother Controls
    The controller is now smoother and the code is a little bit more clean. Air is now being controlled via Raycast (WOAH!)
    All three prefabs were updated with their animation times. And animators post death transition was changed.
    Health is now being drawn as icon x hp. Added mana draw to the EL.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,14 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
 
     // Public Movement Members
-    public float runSpeed = 10f;
+    public float runSpeed = 12f;
     public float jumpForce = 250f;
     [Range(0, 0.3f)] public float movementSmoothing = 0.05f;
 
     // Private Movement Members
     private float horizontalMove = 0f;
     private bool isPreAirborn = false;
+    private float lastDistance = 0f;
 
     // Public Ground Members
     public LayerMask whatIsGround;
@@ -26,7 +27,6 @@ public class PlayerController : MonoBehaviour
 
     // Private Ground Members
     const float groundOverlapRadius = 0.2f;
-    private bool isGrounded;
     private bool isFacingRight = true;
     private Vector3 velocity = Vector3.zero;
 
@@ -39,10 +39,10 @@ public class PlayerController : MonoBehaviour
 
     // Public Animation Timers
     public float jumpAnimTime = 0.25f;
-    public float leftClickAnimTime = 0.35f;
-    public float rightClickAnimTime = 0.35f;
-    public float hurtAnimTime = 0.15f;
-    public float deathAnimTime = 0.72f;
+    public float leftClickAnimTime = 0.7f;
+    public float rightClickAnimTime = 0.7f;
+    public float hurtAnimTime = 0.25f;
+    public float deathAnimTime = 2f;
 
     // Animator Flags
     private bool isJumping = false;
@@ -53,7 +53,7 @@ public class PlayerController : MonoBehaviour
     // --------
     // Starters
     // --------
-    void Start()
+    protected virtual void Start()
     {
         health = maxHealthCount;
     }
@@ -61,15 +61,6 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundChecker.position, groundOverlapRadius, whatIsGround);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject != gameObject)
-            {
-                isGrounded = true;
-            }
-        }
     }
 
     // -------------------
@@ -81,13 +72,13 @@ public class PlayerController : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal");
 
         // Key listeners
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
             isLeftClick = false;
             isRightClick = false;
             isJumping = true;
         }
-        else if (!isLeftClick && !isRightClick && !isJumping && horizontalMove == 0)
+        else if (!animator.GetBool("IsLeftClick") && !animator.GetBool("IsRightClick") && !animator.GetBool("IsJumping") && horizontalMove == 0)
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
@@ -97,6 +88,10 @@ public class PlayerController : MonoBehaviour
             {
                 isRightClick = true;
             }
+        }
+        else if (animator.GetBool("IsLeftClick") || animator.GetBool("IsRightClick"))
+        {
+            horizontalMove = 0;
         }
 
         // Animator speed flag set
@@ -108,42 +103,126 @@ public class PlayerController : MonoBehaviour
         // Resolve hurt effect set hurt flag to false
         if (isBeingHurt)
         {
+            animator.SetBool("IsBeingHurt", true);
+
             GotHit();
+            isBeingHurt = false;
+   
             StartCoroutine(OnHurtAnimation());
         }
+
+        // Check for danger zone
+        CheckDangerZone();
 
         // Resolve death due to danger zone or enough hits
         if (!IsAlive())
         {
             animator.SetBool("IsAlive", false);
             animator.SetBool("IsJumping", false);
-            StartCoroutine(OnDeathAnimation());
 
+            StartCoroutine(OnDeathAnimation());
             return;
         }
 
-        if (!isPreAirborn)
+        // Check for jumps and grounds
+        if (!AirControl())
         {
-            // Ground checking with circlecast
-            bool wasGrounded = isGrounded;
-            isGrounded = false;
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(groundChecker.position, groundOverlapRadius, whatIsGround);
-            for (int i = 0; i < colliders.Length; i++)
+            if (isLeftClick) // If left click was requested
             {
-                if (colliders[i].gameObject != gameObject)
+                animator.SetBool("IsLeftClick", true);
+
+                LeftClick();
+                isLeftClick = false;
+
+                StartCoroutine(OnLeftClickAnimation());
+            }
+            else if (isRightClick) // If right click was requested
+            {
+                animator.SetBool("IsRightClick", true);
+
+                RightClick();
+                isRightClick = false;
+
+                StartCoroutine(OnRightClickAnimation());
+            }
+            else if (!isJumping)
+            {
+                // Move the player on the ground while not jumping or casting
+                Move();
+            }
+        }
+
+        isJumping = false;
+
+        // Refresh player stats if implemented
+        Refresh();
+    }
+
+    // -----------
+    // Controllers
+    // -----------
+    void Move()
+    {
+        float move = horizontalMove * Time.fixedDeltaTime * runSpeed;
+
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(move * 10f, rigidBody.velocity.y);
+
+        // Smoothing the velocity out and applying it to the character
+        rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+        // If the input is moving the player right and the player is facing left, or the opposite, FLIP
+        if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight))
+        {
+            isFacingRight = !isFacingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    bool AirControl()
+    {
+        // Check if truly on ground
+        if (rigidBody.velocity.y <= 0)
+        {
+            // Raycast from the feet of the player directly down
+            RaycastHit2D hit2D = Physics2D.Raycast(groundChecker.position, Vector2.down, 0.2f, whatIsGround);
+
+            // If the raycast hit something
+            if (hit2D && !isPreAirborn)
+            {
+                // Check if the distance of the object hit is less than the last distance checked
+                if (hit2D.distance < lastDistance)
                 {
-                    if (!wasGrounded && rigidBody.velocity.y < 0f)
-                    {
-                        OnLanding();
-                    }
-                    else if (wasGrounded)
-                    {
-                        isGrounded = true;
-                    }
+                    // Update the last distance if the object below is less than the last known distance
+                    lastDistance = hit2D.distance;
+                }
+                else if (isJumping)
+                {
+                    // If the hit distance is not less than the lass distance, then jump (he isn't going to go any lower)
+                    animator.SetBool("IsJumping", true);
+
+                    isPreAirborn = true;
+
+                    StartCoroutine(OnJumpAnimation());
+                }
+                else if (rigidBody.velocity.y == 0)
+                {
+                    animator.SetBool("IsJumping", false);
+
+                    return false;
                 }
             }
         }
 
+        return true;
+    }
+
+    void CheckDangerZone()
+    {
         // Check danger zone collision
         Collider2D[] dangerColliders = Physics2D.OverlapCircleAll(groundChecker.position, groundOverlapRadius, whatIsDangerZone);
         for (int i = 0; i < dangerColliders.Length; i++)
@@ -152,79 +231,6 @@ public class PlayerController : MonoBehaviour
             {
                 health = 0;
             }
-        }
-
-        // Left Click if requested
-        if (isGrounded)
-        {
-            if (isLeftClick)
-            {
-                LeftClick();
-                animator.SetBool("IsLeftClick", true);
-                isLeftClick = false;
-                StartCoroutine(OnLeftClickAnimation());
-            }
-            else if (isRightClick) // Right Click if requested
-            {
-                RightClick();
-                animator.SetBool("IsRightClick", true);
-                isRightClick = false;
-                StartCoroutine(OnRightClickAnimation());
-            }
-            else
-            {
-                // Move the player on the ground if not left or right clicks
-                Move();
-            }
-        }
-
-        // Refresh player stats if implemented
-        Refresh();
-
-        animator.SetBool("IsJumping", isJumping);
-        animator.SetBool("IsBeingHurt", isBeingHurt);
-    }
-
-    // -----------
-    // Controllers
-    // -----------
-    public void Move()
-    {
-        if (health == 0)
-        {
-            return;
-        }
-
-        float move = horizontalMove * Time.fixedDeltaTime * runSpeed;
-
-        // Move the player by input if on ground
-        if (isGrounded)
-        {
-            // Move the character by finding the target velocity
-            Vector3 targetVelocity = new Vector2(move * 10f, rigidBody.velocity.y);
-
-            // Smoothing the velocity out and applying it to the character
-            rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, movementSmoothing);
-
-            // If the input is moving the player right and the player is facing left, or the opposite, FLIP
-            if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight))
-            {
-                isFacingRight = !isFacingRight;
-
-                // Multiply the player's x local scale by -1.
-                Vector3 theScale = transform.localScale;
-                theScale.x *= -1;
-                transform.localScale = theScale;
-            }
-        }
-
-        // If jump was requested
-        if (isGrounded && isJumping && !isPreAirborn)
-        {
-            // Add a vertical force to the player
-            isGrounded = false;
-            isPreAirborn = true;
-            StartCoroutine(OnJumpAnimation());
         }
     }
 
@@ -235,12 +241,6 @@ public class PlayerController : MonoBehaviour
     // ------
     // Events
     // ------
-    public void OnLanding()
-    {
-        isJumping = false;
-        isGrounded = true;
-    }
-
     public void OnGettingHit()
     {
         isBeingHurt = true;
@@ -250,8 +250,9 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(jumpAnimTime);
 
-        isPreAirborn = false;
         rigidBody.AddForce(new Vector2(0f, jumpForce));
+        isPreAirborn = false;
+        lastDistance = 100f;
     }
 
     IEnumerator OnLeftClickAnimation()
@@ -272,7 +273,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(hurtAnimTime);
 
-        isBeingHurt = false;
+        animator.SetBool("IsBeingHurt", false);
     }
 
     IEnumerator OnDeathAnimation()
@@ -305,17 +306,12 @@ public class PlayerController : MonoBehaviour
     // ---
     protected virtual void OnGUI()
     {
-        // Text Style
-        GUIStyle style = new GUIStyle();
-        style.fontSize = 80;
-        style.fontStyle = FontStyle.Bold;
-        style.normal.textColor = Color.yellow;
-
         // Health
-        Rect healthIcon = new Rect(10, 10, 96, 96);
-        Rect healthCountLable = new Rect(healthIcon.xMax, healthIcon.y, 96, 64);
-
-        GUI.DrawTexture(healthIcon, healthTexture);
-        GUI.Label(healthCountLable, health.ToString(), style);
+        Rect healthIcon = new Rect(50, 10, 64, 64);
+        for (int i = 1; i <= health; i++)
+        {
+            GUI.DrawTexture(healthIcon, healthTexture);
+            healthIcon.x += healthIcon.width + 10;
+        }
     }
 }
