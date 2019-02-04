27a578e32c944625eb5380264a760dcb166d62d1
LiadCohen <cohenliad1@walla.com>
2019-02-04 22:38:05 +0200

    Elementalist Scripting
    Elementalist now casts fire / ice on left / right clicks, and during the cast animation the ball goes on to its way for a period of time (or until collides with something from the UI).
    Mana decreases on use, and refreshes after few seconds.
    Added animators for fire and ice, but currently the sprites for ice are pretty lame (fire with color change).
    Also changed the health and mana pngs.

--- Assets/Scripts/Elementalist/ElementalistController.cs
+++ Assets/Scripts/Elementalist/ElementalistController.cs
@@ -11,21 +11,84 @@ public class ElementalistController : PlayerController
 
     // Private Stats Members
     private int mana = 3;
+    private float nextManaRefresh = 0.0f;
+
+    // Public ElementalBall Members
+    public GameObject fireBallToRight, fireBallToLeft;
+    public GameObject iceBallToRight, iceBallToLeft;
+
+    // Private ElementalBall Members
+    private float fireBallWaitTime;
+    private float iceBallWaitTime;
+    private float xBallPosition = 0.7f;
 
     new void Start()
     {
         base.Start();
+
         mana = manaMaxCount;
+
+        fireBallWaitTime = base.leftClickAnimTime - 0.33f;
+        iceBallWaitTime = base.rightClickAnimTime - 0.33f;
     }
 
     // -----------
     // Controllers
     // -----------
-    protected override void LeftClick() { }
+    protected override void LeftClick()
+    {
+        Cast(fireBallToRight, fireBallToLeft, fireBallWaitTime);
+    }
 
-    protected override void RightClick() { }
+    protected override void RightClick()
+    {
+        Cast(iceBallToRight, iceBallToLeft, iceBallWaitTime);
+    }
 
-    protected override void Refresh() { }
+    protected override void Refresh()
+    {
+        // Refresh mana if needed and after set period of time
+        if ((Time.time > nextManaRefresh) && (mana < manaMaxCount))
+        {
+            nextManaRefresh = Time.time + manaRefreshRate;
+            mana++;
+        }
+    }
+
+    void Cast(GameObject elementalToRight, GameObject elementalToLeft, float waitTime)
+    {
+        // If can cast
+        if (mana > 0)
+        {
+            mana--;
+
+            // If first time casting, put the refresh time from this point
+            if (nextManaRefresh == 0)
+            {
+                nextManaRefresh = Time.time + manaRefreshRate;
+            }
+
+            // Coroutine of casting to wait a little before creating the elemental ball (so the animation would get to the right frame)
+            if (base.isFacingRight)
+            {
+                StartCoroutine(OnCast(elementalToRight, xBallPosition, waitTime));
+            }
+            else
+            {
+                StartCoroutine(OnCast(elementalToLeft, (-1) * xBallPosition, waitTime));
+            }    
+        }
+    }
+
+    IEnumerator OnCast(GameObject elemental, float x_position, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Vector2 elementalPosition = transform.position;
+
+        elementalPosition += new Vector2(x_position, 0f);
+        Instantiate(elemental, elementalPosition, Quaternion.identity);
+    }
 
     // ---
     // GUI
@@ -35,7 +98,7 @@ public class ElementalistController : PlayerController
         base.OnGUI();
 
         // Mana
-        Rect manaIcon = new Rect(Screen.width - 100, 10, 64, 64);
+        Rect manaIcon = new Rect(Screen.width - 100, 10, 70, 108);
         for (int i = 1; i <= mana; i++)
         {
             GUI.DrawTexture(manaIcon, manaTexture);
