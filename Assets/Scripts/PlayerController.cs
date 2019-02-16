bb0ca674f5478c9bfc07e494220c55f0630a9c0e
Itay Ovadia <itay.ovadi@gmail.com>
2019-02-17 00:34:12 +0200

    Fixed the jump bug of the Warlord
    - Edited the problematic pictures of the warlord jump
    - Moved the animation to happen after we do the actually jump (instead of before)
    - Removed the animation time of the jump to the prefab
    - Added transitions to some of the animation
    - Some changes to the .idea folder and plugin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -217,7 +217,7 @@ public class PlayerController : MonoBehaviour
                 else if (isJumping)
                 {
                     // If the hit distance is not less than the lass distance, then jump (he isn't going to go any lower)
-                    animator.SetBool("IsJumping", true);
+                    
 
                     isPreAirborn = true;
 
@@ -273,9 +273,11 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(jumpAnimTime);
 
-        rigidBody.AddForce(new Vector2(0f, jumpForce));
+        rigidBody.AddForce(Vector2.up * jumpForce);
         isPreAirborn = false;
         lastDistance = 100f;
+        
+        animator.SetBool("IsJumping", true);
     }
 
     IEnumerator OnLeftClickAnimation()
