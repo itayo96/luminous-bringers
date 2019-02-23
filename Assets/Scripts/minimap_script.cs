40f9da8c7ce2fa1221487bc95abdf3b86a0d7c35
Itay Ovadia <itay.ovadi@gmail.com>
2019-02-24 00:59:02 +0200

    Fixed the north scene minimap
    - Added a green dot to be the player
    - Fixed the border of the minimap
    - Made the minimap to move only on the X asix
    - Natural bg for the minimap

---
+++ Assets/Scripts/minimap_script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimap_script : MonoBehaviour
+{
+    
+    public Transform targetTransform;
+    Vector3 tempVec3 = new Vector3();
+
+    void LateUpdate() {
+        // Find a better way to do this
+        if (targetTransform.position.x > 152)
+        {
+            tempVec3.x = 152 + 40;
+        }
+        else
+        {
+            tempVec3.x = targetTransform.position.x + 40;
+        }
+
+        tempVec3.y = this.transform.position.y;
+        tempVec3.z = this.transform.position.z;
+        this.transform.position = tempVec3;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
