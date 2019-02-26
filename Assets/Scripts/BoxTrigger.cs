da59506eeb3f6a69a835530296028f690698ef60
LiadCohen <cohenliad1@walla.com>
2019-02-26 19:53:46 +0200

    Box Trigger
    Now in the desert scene there is a place where the player needs to move a platform to trigger activating another platform.

---
+++ Assets/Scripts/BoxTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour
+{
+    public GameObject box;
+
+    void Start()
+    {
+        box.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        box.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        box.SetActive(false);
+    }
+}
