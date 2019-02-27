a295d12d6aa5f28e49721b73fdce03c0d8b16cc5
Itay Ovadia <itay.ovadi@gmail.com>
2019-02-27 18:56:16 +0200

    Added a free view camera to the desert scene

---
+++ Assets/Scripts/CameraSelector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CameraSelector : MonoBehaviour
+{
+    public List<Camera> cameras;
+
+    private int activeCameraIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeCameraIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            activeCameraIndex++;
+            activeCameraIndex %= cameras.Count;
+            
+            switchCamera();
+        }
+    }
+
+    void switchCamera()
+    {
+         PlayerController.isInputEnabled = (activeCameraIndex == 0);
+         FreeViewCameraScript.isActive = (activeCameraIndex == 1);
+
+        for (int i = 0; i < cameras.Count; i++)
+        {
+            cameras[i].enabled = (activeCameraIndex == i);
+        }
+    }
+}
